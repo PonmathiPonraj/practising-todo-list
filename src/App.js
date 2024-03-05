008641f1f892ba979e8099ac2766988aec270541
PonmathiPonraj <ponmathittl@gmail.com>
2024-03-04 18:17:32 -0800

    fetch api & CRUD operation

--- src/App.js
+++ src/App.js
@@ -4,35 +4,83 @@ import AddItem from './AddItem';
 import Content from './Content';
 import Footer from './Footer';
 import { useEffect, useState } from 'react';
+import apiRequest from './apiRequest';
 
 function App() {
+  const API_URL = 'http://localhost:3000/items';
   const [items, setItems] = useState([]);
-  
   const [newItem, setNewItem] = useState('');
   const [search, setSearch] = useState('');
+  const [fetchError, setFetchError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    JSON.parse(localStorage.getItem('todo_list'))
+    const fetchItems = async () => {
+      try{
+        const response = await fetch(API_URL);
+        if(!response.ok) throw Error("Data not received");
+        const listItems = await response.json();
+        console.log(listItems);
+        setItems(listItems);
+        setFetchError(null);
+      }
+      catch(err){
+        setFetchError(err.message)
+      }
+      finally{
+        setIsLoading(false)
+      }
+    }
+    setTimeout(() => {
+      (async () => await fetchItems()) ()
+    }, 2000)
   }, [])
 
-  const addItem = (item) => {
+  const addItem = async (item) => {
     const id = items.length ? items[items.length - 1].id + 1 : 1;
     const addNewItem = {id, checked:false, item}
     const listItems = [...items, addNewItem]
     setItems(listItems)
-    localStorage.setItem('todo_list', JSON.stringify(listItems))
+    
+    const postOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(addNewItem)
+    }
+
+    const result = await apiRequest(API_URL, postOptions)
+    if(result) setFetchError(result)
   }
 
-  const handleCheck = (id) => {
+  const handleCheck = async (id) => {
     const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
     setItems(listItems);
-    localStorage.setItem('todo_list', JSON.stringify(listItems))
+
+    const myItem = listItems.filter((item) => item.id === id);
+    const updateOptions = {
+      method: 'PATCH',                //PATCH _ update the data
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({checked: myItem[0].checked})
+    }
+
+    const reqUrl = `$(API_URL)/${id}`
+    const result = await apiRequest(reqUrl, updateOptions)
+    if(result) setFetchError(result)
   }
 
-  const handleDelete = (id) => {
+  const handleDelete = async(id) => {
     const listItems = items.filter((item) => item.id !== id );
     setItems(listItems);
-    localStorage.setItem('todo_list', JSON.stringify(listItems))
+
+    const deleteOptions = { method: 'DELETE' }
+
+    const reqUrl = `$(API_URL)/${id}`
+    const result = await apiRequest(reqUrl, deleteOptions)
+    if(result) setFetchError(result)
   }
 
   const handleSubmit = (e) => {
@@ -44,7 +92,7 @@ function App() {
 
   return (
     <div className="App">
-      <Header title="To do List" />
+      <Header title="Course List" />
       <AddItem
         newItem={newItem}
         setNewItem={setNewItem}
@@ -54,11 +102,15 @@ function App() {
         search={search}
         setSearch={setSearch}
       />
-      <Content 
-        items={items.filter(item=>((item.item).toLowerCase()).includes(search.toLowerCase()))}
-        handleCheck={{handleCheck}}
-        handleDelete={handleDelete}
-      />
+      <main>
+        {isLoading && <p>Loading Items...</p>}
+        {fetchError && <p>{`Error: ${fetchError}`}</p>}
+        {!isLoading && !fetchError && <Content 
+          items={items.filter(item=>((item.item).toLowerCase()).includes(search.toLowerCase()))}
+          handleCheck={{handleCheck}}
+          handleDelete={handleDelete}
+        />}
+      </main>
       <Footer length={items.length} />
     </div>
   );
--- src/Content.js
+++ src/Content.js
@@ -13,6 +13,7 @@ const Content = ({ items, handleCheck, handleDelete }) => {
                 <p style={{ marginTop: '2rem' }}>Your list is empty.</p>
             )}
         </>
+        
     )
 }
 
